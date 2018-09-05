3014619c43e11e622e98f701f9900c8b6ce251e6
hweeeeeei <hwei1233@163.com>
2018-09-05 17:14:17 +0800

    1.修改注解 2.添加断路器

---
+++ hystrix-demo1/src/test/java/com/hw/hystrixdemo1/HystrixDemo1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hw.hystrixdemo1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HystrixDemo1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
