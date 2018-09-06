6f8ada9d93c563d997a16464d8de5f1e964379ba
hweeeeeei <hwei1233@163.com>
2018-09-07 00:18:43 +0800

    add: 配置中心

---
+++ config-server-demo1/src/main/java/com/hw/configserverdemo1/ConfigServerDemo1Application.java
@@ -0,0 +1,12 @@
+package com.hw.configserverdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerDemo1Application.class, args);
+    }
+}
