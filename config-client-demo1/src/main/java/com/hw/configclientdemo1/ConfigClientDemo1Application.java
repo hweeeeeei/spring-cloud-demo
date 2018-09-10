47fbeedda2151b80169787d206f9d9a478ef01e6
hweeeeeei <hwei1233@163.com>
2018-09-10 14:19:37 +0800

    add: 配置中心config-server &  config-client

---
+++ config-client-demo1/src/main/java/com/hw/configclientdemo1/ConfigClientDemo1Application.java
@@ -0,0 +1,12 @@
+package com.hw.configclientdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientDemo1Application.class, args);
+    }
+}
--- config-server-demo1/src/main/java/com/hw/configserverdemo1/ConfigServerDemo1Application.java
+++ config-server-demo1/src/main/java/com/hw/configserverdemo1/ConfigServerDemo1Application.java
@@ -2,11 +2,17 @@ package com.hw.configserverdemo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * EnableConfigServer 注解开启配置服务器的功能
+ */
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerDemo1Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ConfigServerDemo1Application.class, args);
     }
 }
