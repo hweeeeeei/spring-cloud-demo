f723916393bdd7998ec6d39a8db27187fa37fab1
hweeeeeei <hwei1233@163.com>
2018-09-10 15:56:19 +0800

    配置中心

--- config-client-demo1/src/main/java/com/hw/configclientdemo1/ConfigClientDemo1Application.java
+++ config-client-demo1/src/main/java/com/hw/configclientdemo1/ConfigClientDemo1Application.java
@@ -1,12 +1,32 @@
 package com.hw.configclientdemo1;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *  配置中心客户端
+
+ */
 @SpringBootApplication
+@RestController
 public class ConfigClientDemo1Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ConfigClientDemo1Application.class, args);
+    }
+
+
+
+    @Value("${hi}")
+    String message;
+
+    @GetMapping("/hi")
+    public String hiMessage(){
+
+        return message;
     }
 }
