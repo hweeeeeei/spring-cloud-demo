17b0dca49833a8b6923b1f2291e722cd4830dc41
hweeeeeei <hwei1233@163.com>
2018-09-10 18:04:46 +0800

    updata配置中心

--- config-client-demo1/src/main/java/com/hw/configclientdemo1/ConfigClientDemo1Application.java
+++ config-client-demo1/src/main/java/com/hw/configclientdemo1/ConfigClientDemo1Application.java
@@ -3,6 +3,7 @@ package com.hw.configclientdemo1;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class ConfigClientDemo1Application {
 
     public static void main(String[] args) {
--- config-server-demo1/src/main/java/com/hw/configserverdemo1/ConfigServerDemo1Application.java
+++ config-server-demo1/src/main/java/com/hw/configserverdemo1/ConfigServerDemo1Application.java
@@ -3,12 +3,14 @@ package com.hw.configserverdemo1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * EnableConfigServer 注解开启配置服务器的功能
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerDemo1Application {
 
     public static void main(String[] args) {
